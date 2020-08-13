c6e2e35eae2df36e3d9d88ab3b74d1b940323c77
Chantha Tith <ctithservices@gmail.com>
2020-08-12 21:57:02 -0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
+import { axios } from "axios";
+
+//import gql from "graphql-tag";
+
+
+const client = new ApolloClient({
+  uri: 'https://bluemaxapi20200812102838.azurewebsites.net/api/graphql',
+  cache: new InMemoryCache()
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        bluemax {
+          fullName,
+          awardActionDate
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
